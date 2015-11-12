30904e84d17ca88b35f88cb779f773375c4ba2a7
Ryan Walker <ryan@maddenwalker.com>
2015-11-12 14:30:12 -0800

    Stub out buttons in IB

---
+++ Swiftris/Swiftris/MainMenuViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainMenuViewController.swift
+//  Swiftris
+//
+//  Created by Ryan Walker on 11/12/15.
+//  Copyright © 2015 Ryan Walker. All rights reserved.
+//
+
+import UIKit
+
+class MainMenuViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    
+}
